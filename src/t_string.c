cded3c834a9dfc259d10845c9f535677fd549834
chenfaming <chenfanming@houbank.com>
2017-03-17 11:45:01 +0800

    sdscat会修改原字符串s 使用sdsdup复制一份

--- src/t_string.c
+++ src/t_string.c
@@ -474,7 +474,7 @@ void strlenCommand(client *c) {
 }
 
 void settreeCommand(client *c) {
-	sds* key = (sds*)c->argv[1]->ptr;
+	sds key = sdsdup(*((sds*)c->argv[1]->ptr));
 	sds* value = (sds*)c->argv[2]->ptr;
 	sds msg = sdscatsds(*key, *value);
 	addReplyBulkSds(c, msg);
